960761ffbbe230c066fc55685b3822549f13a611
nivash77 <151871666+nivash77@users.noreply.github.com>
2024-10-21 21:48:48 +0530

    modified of copy and paste

--- Frontend/src/components/Answer.jsx
+++ Frontend/src/components/Answer.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect,useRef } from "react";
 import { Editor as MonacoEditor } from "@monaco-editor/react";
 import { MdOutlineArrowUpward, MdOutlineArrowDownward } from "react-icons/md";
 import { FaPlay, FaSun, FaMoon } from "react-icons/fa";
@@ -6,11 +6,39 @@ import "../assets/css/Answer.css";
 
 const Answer = ({ currentIndex, language, code, onCodeChange }) => {
   const [theme, setTheme] = useState("vs-dark");
+  const [TextCode, setCode] = useState('// Write your code here...');
   const [showOutput, setShowOutput] = useState(false);
   const [customInput, setCustomInput] = useState("");
   const [expectedOutput, setExpectedOutput] = useState("5,10,15");
   const [gotOutput, setGotOutput] = useState("");
   const [compilationMessage, setCompilationMessage] = useState("");
+  const previousCode = useRef('');
+  let textareaCode=TextCode;
+  const [previousCodetextarea,setPreviousCodetextarea]=useState(TextCode);
+  const handleOnChange = (value) => {
+    const filteredValue = value.replace(/[\s\t\[\]{}()<>.,;:'"\\#@!$%^&*+=~`/]/g, '');
+    console.log('Filtered code:', filteredValue);
+    console.log('Previous code:', previousCode.current);
+
+    let lengthDifference = filteredValue.length - previousCode.current.length;
+    console.log(lengthDifference);
+    
+    if (lengthDifference > 3 && lengthDifference !== 0) {
+      alert("You pasted a code");
+      setCode(previousCodetextarea); 
+      setPreviousCodetextarea(value)
+      console.log("Setting code to previousCodetextarea:", previousCodetextarea);
+    } else {
+      setPreviousCodetextarea(TextCode);
+      console.log("Setting code to current value:", value);
+     textareaCode=value;
+     setCode(value);
+    }
+    console.log("Code in textarea:",textareaCode);
+    
+    previousCode.current = filteredValue;
+  };
+
 
   const compileCode = async () => {
     setShowOutput(true);
@@ -47,7 +75,7 @@ const Answer = ({ currentIndex, language, code, onCodeChange }) => {
         >
           {theme === "vs-dark" ? <FaSun size={18} /> : <FaMoon size={18} />}
         </button>
-        
+
       </div>
 
       <div className="editor-container mb-4 border border-gray-300 rounded-md shadow-lg">
@@ -56,8 +84,10 @@ const Answer = ({ currentIndex, language, code, onCodeChange }) => {
           width="100%"
           defaultLanguage={language}
           theme={theme}
-          value={code}
-          onChange={(value) => onCodeChange(value || "")} 
+          // value={code}
+          // onChange={(value) => onCodeChange(value || "")} 
+          value={TextCode}
+          onChange={handleOnChange} 
           options={{
             lineNumbers: "on",
             tabSize: 2,
