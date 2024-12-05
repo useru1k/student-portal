c50471cf0be1f4e29afe9492b1f7165623d8f7eb
Yuvan krishna P <143686903+useru1k@users.noreply.github.com>
2024-12-05 09:39:59 +0530

    Updating a UI of Upcoming Event - phase 2

--- Frontend/src/pages/UpcomingEvents.jsx
+++ Frontend/src/pages/UpcomingEvents.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// Need to Connect to DB
 const eventsData = [
   {
     id: 1,
@@ -37,13 +38,67 @@ const eventsData = [
     date: 'November 1, 2024',
     time: '11:00 AM - 1:00 PM',
   },
+  {
+    id: 4,
+    title: 'Web Development Test-I',
+    date: 'October 20, 2024',
+    time: '10:00 AM - 2:00 PM',
+  },
+  {
+    id: 5,
+    title: 'Web Development Test-I',
+    date: 'October 20, 2024',
+    time: '10:00 AM - 2:00 PM',
+  },
+  {
+    id: 6,
+    title: 'Web Development Test-I',
+    date: 'October 20, 2024',
+    time: '10:00 AM - 2:00 PM',
+  },
+  {
+    id: 7,
+    title: 'Web Development Test-I',
+    date: 'October 20, 2024',
+    time: '10:00 AM - 2:00 PM',
+  },
+  {
+    id: 8,
+    title: 'Web Development Test-I',
+    date: 'October 20, 2024',
+    time: '10:00 AM - 2:00 PM',
+  },
+  {
+    id: 9,
+    title: 'Web Development Test-I',
+    date: 'October 20, 2024',
+    time: '10:00 AM - 2:00 PM',
+  },
+  {
+    id: 10,
+    title: 'Web Development Test-I',
+    date: 'October 20, 2024',
+    time: '10:00 AM - 2:00 PM',
+  },
+  {
+    id: 11,
+    title: 'Web Development Test-I',
+    date: 'October 20, 2024',
+    time: '10:00 AM - 2:00 PM',
+  },
+  {
+    id: 12,
+    title: 'Web Development Test-I',
+    date: 'October 20, 2024',
+    time: '10:00 AM - 2:00 PM',
+  },
   // Add more events here as needed
 ];
 
 const UpcomingEvents = () => {
   return (
-    <div className="h-screen overflow-y-auto p-6 custom-scroll bg-gray-100">
-      <h2 className="text-2xl font-bold mb-4">Upcoming Events</h2>
+    <div className="h-[85vh] p-6 bg-gray-100 overflow-y-auto custom-scroll">
+      <h2 className="text-2xl font-bold mb-5">Upcoming Events</h2>
       <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3">
         {eventsData.map(event => (
           <div key={`${event.id}-${event.date}`} className="bg-white shadow-lg rounded-lg p-6 transform transition-transform duration-300 hover:scale-105 hover:shadow-xl">
@@ -59,5 +114,4 @@ const UpcomingEvents = () => {
   );
 };
 
-	
 export default UpcomingEvents;
