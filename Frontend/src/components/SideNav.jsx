14631c63462469b00ec4442bb42b0f3fe92d583c
Yuvan krishna P <143686903+useru1k@users.noreply.github.com>
2024-12-06 12:14:57 +0530

    Update SideNav

--- Frontend/src/components/SideNav.jsx
+++ Frontend/src/components/SideNav.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { HomeIcon, BookOpenIcon, UserIcon } from 'lucide-react'; 
+import { HomeIcon, BookOpenIcon, UserIcon } from 'lucide-react';
 import { MdLeaderboard, MdQueuePlayNext } from 'react-icons/md';
 
 const SideNav = ({ isOpen }) => {
   return (
+    // isOpen - When chick the bar means Adjust the screen Size
     <div className={`bg-[#293643] text-white h-full fixed top-16 transition-transform duration-300 ${isOpen ? 'w-64' : 'w-0'} overflow-hidden`}>
       <ul className="mt-4">
         <Link to="/dashboard/courses" className="flex hover:bg-gray-700 items-center">
@@ -25,12 +26,6 @@ const SideNav = ({ isOpen }) => {
             <span>Leader Board</span>
           </li>
         </Link>
-        {/* <Link to="/dashboard/profile" className="flex hover:bg-gray-700 items-center">
-          <li className="p-4 flex items-center transition-colors duration-200">
-            <UserIcon className="mr-2" size={23} />
-            <span>Profile</span>
-          </li>
-        </Link> */}
         <Link to="/dashboard/siethome" className="flex hover:bg-gray-700 items-center">
           <li className="p-4 flex items-center transition-colors duration-200">
             <HomeIcon className="mr-2" size={23} />
